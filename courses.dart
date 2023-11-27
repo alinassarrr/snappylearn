6c8213505dae2468bbfd43df1cdf4610da9ef7dc
Qusay17 <152025080+Qusay17@users.noreply.github.com>
2023-11-27 22:43:38 +0200

    Update courses.dart

--- courses.dart
+++ courses.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 
-
-class CoursesPage extends StatelessWidget {
+class CoursesPage extends StatefulWidget {
   final String major;
   final Map<String, Widget> coursePages;
+  final Function(String) onCourseFavoriteToggle;
 
-  CoursesPage({required this.major, required this.coursePages});
+  CoursesPage(
+      {required this.major,
+      required this.coursePages,
+      required this.onCourseFavoriteToggle});
+
+  @override
+  _CoursesPageState createState() => _CoursesPageState();
+}
+
+class _CoursesPageState extends State<CoursesPage> {
+  // A set to keep track of favorited courses
+  Set<String> favoritedCourses = Set<String>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Courses of - $major'),
+        title: Text('Courses of - ${widget.major}'),
       ),
       body: ListView.builder(
-        itemCount: coursePages.length,
+        itemCount: widget.coursePages.length,
         itemBuilder: (context, index) {
-          var courseName = coursePages.keys.elementAt(index);
+          var courseName = widget.coursePages.keys.elementAt(index);
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: ElevatedButton(
@@ -24,7 +35,7 @@ class CoursesPage extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => coursePages[courseName]!,
+                    builder: (context) => widget.coursePages[courseName]!,
                   ),
                 );
               },
@@ -33,15 +44,48 @@ class CoursesPage extends StatelessWidget {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(8.0),
                 ),
-                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+                fixedSize: Size(200, 125), // Set the size as needed
               ),
-              child: Text(
-                courseName,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25.0,
-                ),
+              child: Stack(
+                children: [
+                  // Centered course name
+                  Center(
+                    child: Text(
+                      courseName,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30.0,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  // Star button at bottom right
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: IconButton(
+                      icon: favoritedCourses.contains(courseName)
+                          ? Icon(Icons.star)
+                          : Icon(Icons.star_border),
+                      onPressed: () {
+                        // Toggle the course's favorite status
+                        widget.onCourseFavoriteToggle(courseName);
+
+                        setState(() {
+                          if (favoritedCourses.contains(courseName)) {
+                            favoritedCourses.remove(courseName);
+                          } else {
+                            favoritedCourses.add(courseName);
+                          }
+                        });
+                      },
+                      color: Colors.yellow,
+                      iconSize: 30,
+                    ),
+                  ),
+                ],
               ),
             ),
           );
@@ -50,8 +94,6 @@ class CoursesPage extends StatelessWidget {
     );
   }
 }
-
-
 
 class CoursePage extends StatelessWidget {
   final String courseName;
